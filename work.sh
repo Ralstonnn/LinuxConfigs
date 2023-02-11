518b01b110c7c64374e9dbb1aaebe6fc4898aeef
Ralstonnn <zigthehedge2002@gmail.com>
2023-02-11 16:46:35 +0500

    update

--- work.sh
+++ work.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# The parantathes make each application to open in a virtual shell
 (~/Applications/space_741b11a1202a216e84254df0ba1bc9f6 &)
 (~/.local/share/JetBrains/Toolbox/apps/PhpStorm/ch-0/223.8617.59/bin/phpstorm.sh &) 
 (io.github.Figma_Linux.figma_linux &) 
