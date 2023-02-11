8225d1156e6abe1b00525b0e3e91be1e99380a7c
Ralstonnn <zigthehedge2002@gmail.com>
2023-02-11 16:36:12 +0500

    initial commit

---
+++ work.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+(~/Applications/space_741b11a1202a216e84254df0ba1bc9f6 &)
+(~/.local/share/JetBrains/Toolbox/apps/PhpStorm/ch-0/223.8617.59/bin/phpstorm.sh &) 
+(io.github.Figma_Linux.figma_linux &) 
